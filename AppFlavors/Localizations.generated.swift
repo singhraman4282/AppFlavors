d7c888ca62578a68001662dea0f6ac9cbe93c58d
Raman Singh <raman.singh@thescore.com>
2022-09-09 21:00:09 -0400

    awesome

--- AppFlavors/Localizations.generated.swift
+++ AppFlavors/Localizations.generated.swift
@@ -5,76 +5,63 @@
 // MARK: Localization
 
 extension L10n.Localizations {
-
     
     static let femaleSuperhero: String = {
-#if DC
-        L10n.DCLocalizations.femaleSuperhero
-#elseif Marvel
-        L10n.MarvelLocalizations.femaleSuperhero
+#if Marvel
+    L10n.MarvelLocalizations.femaleSuperhero
 #else
-        ""
+    L10n.DCLocalizations.femaleSuperhero
 #endif
     }()
-
+    
     static let flyingMan: String = {
-#if DC
-        L10n.DCLocalizations.flyingMan
-#elseif Marvel
-        L10n.MarvelLocalizations.flyingMan
+#if Marvel
+    L10n.MarvelLocalizations.flyingMan
 #else
-        ""
+    L10n.DCLocalizations.flyingMan
 #endif
     }()
-
+    
     static let richMan: String = {
-#if DC
-        L10n.DCLocalizations.richMan
-#elseif Marvel
-        L10n.MarvelLocalizations.richMan
+#if Marvel
+    L10n.MarvelLocalizations.richMan
 #else
-        ""
+    L10n.DCLocalizations.richMan
 #endif
     }()
+    
 }
 
 // MARK: Image Assets
 
 extension Asset {
 
-            
     static let appBanner: ImageAsset = {
         Asset.Assets.appBanner
     }()
-                        
+
     static let femaleSuperhero: ImageAsset = {
-#if DC
-        Asset.DCAssets.femaleSuperhero
-#elseif Marvel
+#if Marvel
         Asset.MarvelAssets.femaleSuperhero
 #else
         Asset.DCAssets.femaleSuperhero
 #endif
     }()
-                    
+
     static let flyingMan: ImageAsset = {
-#if DC
-        Asset.DCAssets.flyingMan
-#elseif Marvel
+#if Marvel
         Asset.MarvelAssets.flyingMan
 #else
         Asset.DCAssets.flyingMan
 #endif
     }()
-                    
+
     static let richGuy: ImageAsset = {
-#if DC
-        Asset.DCAssets.richGuy
-#elseif Marvel
+#if Marvel
         Asset.MarvelAssets.richGuy
 #else
         Asset.DCAssets.richGuy
 #endif
     }()
-                                                    
+
 }
